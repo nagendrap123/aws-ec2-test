2d48c7ed78b8e45c71fa791982bf83e8cf4aa27f
nagendrap123 <nagendrap0820@gmail.com>
2024-07-30 11:50:59 +0530

    module-ec2

--- output.tf
+++
@@ -1,8 +0,0 @@
-output "public_ip" {
-    value = aws_instance.db.public_ip
-}
-
-output "private_ip" {
-    value = aws_instance.db.private_ip
-  
-}
---
+++ outputs.tf
@@ -0,0 +1,8 @@
+output "public_ip" {
+    value = module.ec2-test.public_ip
+}
+output "private_ip" {
+    value = module.ec2-test.private_ip
+  
+}
+
